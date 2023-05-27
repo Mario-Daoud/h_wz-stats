d1449e711593a003a0c50f4d76a1e1c1734d2b4f
Mario Daoud <r0895582@student.vives.be>
2023-05-27 13:51:25 +0200

    add: platform selection icons

--- wz-stats_frontend/src/app/statspage/searchbar/searchbar.component.ts
+++ wz-stats_frontend/src/app/statspage/searchbar/searchbar.component.ts
@@ -19,19 +19,46 @@ export class SearchbarComponent implements OnInit {
 
     sr.reveal('.stats-head', { delay: 200, origin: 'top' });
     sr.reveal('.searchbar-input', { delay: 200, origin: 'bottom' });
-  }
 
+    this.selectImage = '/assets/images/platform-icons/platform-icon.png';
+  }
   playerName: string;
   playerStats: Player;
+  playerPlatform: string;
+  selectImage: string;
 
   constructor(private playerService: PlayerService) {}
 
   getPlayerStats(): void {
     const name = this.playerName.split('#')[0];
     const tag = this.playerName.split('#')[1];
-    const platform = 'Activision'; // Replace 'platform-name' with the desired platform
+    const platform = this.playerPlatform;
     this.playerService
       .getPlayerStats(name, tag, platform)
       .subscribe((playerStats) => (this.playerStats = playerStats));
+  }
+
+  togglePlatform() {
+    var dropdownContent = document.getElementsByClassName(
+      'dropdown-content'
+    )[0] as HTMLElement;
+    if (dropdownContent.style.display === 'block') {
+      dropdownContent.style.display = 'none';
+    } else {
+      dropdownContent.style.display = 'block';
+    }
+  }
+
+  setPlatform(platform: string) {
+    this.playerPlatform = platform;
+    var dropdownContent = document.getElementsByClassName(
+      'dropdown-content'
+    )[0] as HTMLElement;
+
+    dropdownContent.style.display = 'none';
+
+    this.selectImage =
+      '/assets/images/platform-icons/' + platform + '-icon.png';
+    this.togglePlatform();
   }
 }
