cbfe8416c252e6f0940ab5a54b522062764455ed
Mario Daoud <r0895582@student.vives.be>
2023-05-27 02:42:21 +0200

    changed search

--- wz-stats_frontend/src/app/statspage/statspage.module.ts
+++ wz-stats_frontend/src/app/statspage/statspage.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchbarComponent } from './searchbar/searchbar.component';
-import { StatspageComponent } from './statspage/statspage.component';
 import { CoreModule } from '../core/core.module';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [SearchbarComponent, StatspageComponent],
+  declarations: [SearchbarComponent],
   imports: [CommonModule, CoreModule, FormsModule],
-  exports: [StatspageComponent],
+  exports: [SearchbarComponent],
 })
-export class StatspageModule {}
+export class StatspageModule {
+  
+}
